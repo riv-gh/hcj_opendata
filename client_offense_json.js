142805ddbad6bea634581bcb8739de9956b18bcb
daenur <daenur@outlook.com>
2023-04-12 16:16:01 +0300

    Update client_offense_json.js

--- client_offense_json.js
+++ client_offense_json.js
@@ -1,14 +1,14 @@
-var root = document.querySelector('section.col-sm-12>.region.region-content')
-var count = +document.querySelector('.pager-last>a').href.split('=')[1]+1
+const root = document.querySelector('section.col-sm-12>.region.region-content')
+const count = +document.querySelector('.pager-last>a').href.split('=')[1]+1
 
 root.textContent = ''
 
-var links = (new Array(count))
+const links = (new Array(count))
 .fill(0)
 .map((el,i)=>'https://hcj.gov.ua/offense?page='+i)
 
-var main_arr = []
-var err_arr = []
+let main_arr = []
+let err_arr = []
 
 Promise.all(links.map(url =>
     fetch(url)
@@ -25,7 +25,7 @@ Promise.all(links.map(url =>
                 "Назва суду": tr.querySelector('.views-field-field-court-name').textContent.trim(),
                 "Дата прийняття рішення палатою / Дата рішення ВККСУ": tr.querySelector('.views-field-field-date').textContent.trim(),
                 "Номер рішення палати / Номер рекомендації ВККСУ": tr.querySelector('.views-field-field-number').textContent.trim(),
-                "Дата та номер рішення ВРП": tr.querySelector('.views-field-field-date-iso').textContent.trim()+'\n'+tr.querySelector('.views-field-field-number-1').textContent.trim(),
+                "Дата та номер рішення ВРП": tr.querySelector('.views-field-field-date-iso').textContent.trim()+'\n№ '+tr.querySelector('.views-field-field-number-1').textContent.trim(),
                 "Дата рішення ВРП": tr.querySelector('.views-field-field-date-iso').textContent.trim(),
                 "Номер рішення ВРП": tr.querySelector('.views-field-field-number-1').textContent.trim(),
                 "Автор звернення": tr.querySelector('.views-field-field-complainant').textContent.trim(),
@@ -39,6 +39,20 @@ Promise.all(links.map(url =>
     })
 )).then(data => {
     main_arr = main_arr.sort((a,b)=>a["№"]-b["№"])
-    console.log(JSON.stringify(main_arr, null, '\t'))
-});
+    const json = JSON.stringify(main_arr, null, '\t')
+    console.log(json)
+    const pre = document.createElement('pre')
+    root.appendChild(pre)
+    pre.textContent = json
+    download('offense', json);
+})
 
+function download(filename, text) {
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
